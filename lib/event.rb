1ad09134643f2141d1744e2870cfa5d6a3726d8f
Celia Lobet <lobet.c@gmail.com>
2021-04-26 15:51:35 +0200

    first commit

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,15 @@
+require "time"
+require "pry"
+
 class Event
+  attr_accessor :start_date
+
+  def initialize(start_date, duration, title)
+    @start_date = Time.parse(start_date)
+    @duration = duration.to_i
+    @title = title
+  end
 
 end
+binding.pry
+puts "end of file"
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,15 @@
 class User
+  attr_accessor :email, :age
+  @@all_users = Array.new
+
+  def initialize(email_to_save, age_to_save)
+    @email = email_to_save
+    @age = age_to_save.to_i
+    @@all_users << self
+  end
+
+  def self.all
+    return @@all_users
+  end
 
 end
